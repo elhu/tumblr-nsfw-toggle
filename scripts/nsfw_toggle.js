00efb9685b4f473998ba78eb8831341ec7786647
Vincent Boisard <vincent.boisard@dimelo.com>
2015-06-30 23:40:09 +0200

    cleanup + use native icon

--- scripts/nsfw_toggle.js
+++ scripts/nsfw_toggle.js
@@ -1,11 +1,16 @@
 // Building toggle button
 var userTools = document.getElementById('user_tools');
 var toggleDiv = document.createElement("div");
-var toggleIcon = document.createElement("img");
+var toggleIcon = document.createElement("i");
+var toggleWrapper = document.createElement("a");
+
 toggleDiv.classList.add('tab');
 toggleDiv.classList.add('iconic');
 toggleDiv.id = 'tumblr-nsfw-toggle';
-toggleIcon.setAttribute('src', chrome.extension.getURL('images/evil2.png'));
+
+toggleIcon.classList.add('icon_lock');
+toggleIcon.style.fontSize = '22px';
+toggleIcon.style.lineHeight = '30px';
 
 // Adding button to Tumblr UI
 toggleDiv.appendChild(toggleIcon);
@@ -15,14 +20,16 @@ userTools.appendChild(toggleDiv);
 var style = document.createElement('style');
 style.type = 'text/css';
 style.id = "tumblr-nsfw-toggle-style";
-style.appendChild(document.createTextNode('div.post[data-tumblelog-content-rating="nsfw"] .post_media img { visibility: hidden; }'));
+style.appendChild(document.createTextNode('div.post[data-tumblelog-content-rating="nsfw"] .post_media img, div.post[data-tumblelog-content-rating="adult"] .post_media img { visibility: hidden; }'));
 
 // Handling the actual toggle
 toggleDiv.addEventListener('click', function() {
   var styleElt = document.getElementById("tumblr-nsfw-toggle-style");
   if (styleElt == null) {
+    toggleIcon.style.color = "#fff";
     document.getElementsByTagName('head')[0].appendChild(style);
   } else {
+    toggleIcon.style.color = "rgba(255,255,255,.5)";
     styleElt.parentNode.removeChild(styleElt);
   }
 });
